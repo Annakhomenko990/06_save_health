c3533cb2d71dbe0c191b61f477d255b800226acd
Annakhomenko990 <24a_khao@liceum.ztu.edu.ua>
2025-03-18 13:01:11 +0200

    редагування сайту

--- script.js
+++ script.js
@@ -1,62 +1,75 @@
-const arrayOfhealthWishes = [
-    "Exercise regularly – At least 30 minutes a day.",
-    "Get enough sleep – Aim for 7–9 hours per night.",
-    "Manage stress – Try meditation or deep breathing.",
-    "Maintain hygiene – Wash hands and keep clean.",
-    "MStay active – Avoid sitting for long periods.",
-    "Have regular check-ups – Visit your doctor yearly.",
-    "Think positive – A healthy mind means a healthy body.",
-];
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+  arrayOfhealthWishes = [
+    "I wish you strong health and strength!",
+   "May illnesses stay away from you.",
+    "I wish you true love that will always be by your side!",
+    "Live without stress and in harmony with your soul.",
+    "May every day bring you joy!",
+  ];
 
-let countofpills = 5
-document.getElementById('count-of-tablets').innerText ="💊".repeat(countofpills)
-
-
-document.getElementById('btn_health_wishes').addEventListener('click', () => {
-let index = Math.floor(Math.random()* arrayOfhealthWishes.length)
-document.getElementById('p-health-wishes').innerText = arrayOfhealthWishes[index]
-
-countofpills--
-console.log(countofpills)
-
-document.getElementById('count-of-tablets').innerText="💊".repeat(countofpills) + "❌".repeat(5-countofpills);
-console.log("💊".repeat(countofpills) + "❌".repeat(5-countofpills));
-
-if (countofpills == 0){
-console.log("countofpills = 0");
-document.getElementById('btn_health_wishes').style.display = 'none';
- }
-
-});
-
-const arrayOfImages= [
-    "1.jpg",
-    "2.jpg", 
-    "3.jpg",
-    "4.jpg"
-]
-
-document.getElementById('btn_health_wishes').addEventListener('click', () => {
-    let index = Math.floor(Math.random() * arrayOfHealthWishes.length)
-    document.getElementById('p-health-wishes').innerText = arrayOfHealthWishes[index]
-})
-
-document.getElementById("main-image").setAttribute('src', `img/gallery/${arrayOfImages[galleryImage-1]}`)
-
-document.getElementById("right-arrow").addEventListener('click', () => {
-    galleryImage++
-    console.log(galleryImage)
-
-    if(galleryImage == arrayOfImages.length + 1) galleryImage = 1
-
-    document.getElementById("main-image").setAttribute('src', `img/gallery/${arrayOfImages[galleryImage-1]}`)
-})
-
-document.getElementById("left-arrow").addEventListener('click', () => {
-    galleryImage--
-    console.log(galleryImage)
-
-    if (galleryImage == 0) galleryImage = 3
-
-    document.getElementById("main-image").setAttribute('src', `img/gallery/${arrayOfImages[galleryImage-1]}`)
-})
+  
+    document.getElementById("btn_health_wishes").addEventListener("click", () => {
+    document.getElementById("p-health-wishes").innerHTML = arrayOfhealthWishes[getRandomInt(4)];
+    });
+  
+    const arrayOfImages = [
+      "1.jpg",
+      "2.jpg",
+      "3.jpg"
+    ]
+  
+    let countOfpills = 5;
+    document.getElementById("count-of-tablets").innerText = "💊".repeat(
+      countOfpills
+    );
+    document.getElementById("btn_health_wishes").addEventListener("click", () => {
+      let index = Math.floor(Math.random() * arrayOfhealthWishes.length);
+      document.getElementById("p-health-wishes").innerText =
+        arrayOfhealthWishes[index];
+    
+      countOfpills--;
+      console.log(countOfpills);
+      document.getElementById("count-of-tablets").innerText =
+        "💊".repeat(countOfpills) + "❌".repeat(5 - countOfpills);
+      console.log("💊".repeat(countOfpills) + "❌".repeat(5 - countOfpills));
+      if (countOfpills === 0) {
+        console.log("countOfpills = 0");
+        document.getElementById("btn_health_wishes").style.display = "none";
+      }
+    });
+    
+    document.getElementById("btn-buy-tablets").addEventListener("click", () => {
+      countOfpills = 5;
+      console.log(countOfpills);
+      document.getElementById("count-of-tablets").innerText = "💊".repeat(
+        countOfpills
+      );
+      document.getElementById("btn_health_wishes").style.display = "inline-block";
+    });
+    
+    let galleryImage = 1
+  
+    document.getElementById("main-image").setAttribute("src",`img/gallery/${arrayOfImages[galleryImage-1]}`)
+      
+      document.getElementById("right-arrow").addEventListener('click', ()=>{
+        galleryImage++
+        console.log(galleryImage)
+      
+        if(galleryImage == 4)
+           {galleryImage = 1}
+  
+        document.getElementById("main-image").setAttribute("src",`img/gallery/${arrayOfImages[galleryImage-1]}`)
+      })
+  
+      document.getElementById("left-arrow").addEventListener('click', ()=>{
+        galleryImage++
+        console.log(galleryImage)
+      
+        if(galleryImage == 4)
+           {galleryImage = 1}
+  
+        document.getElementById("main-image").setAttribute("src",`img/gallery/${arrayOfImages[galleryImage-1]}`)
+      })
